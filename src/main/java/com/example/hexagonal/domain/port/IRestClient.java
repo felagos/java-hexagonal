f4813ff54a1e937608c0190a6fa681660057817b
Felipe Lagos <felagossanchez@gmail.com>
2025-04-26 20:12:34 -0400

    style: Remove unnecessary whitespace in IRestClient interface

--- src/main/java/com/example/hexagonal/domain/port/IRestClient.java
+++ src/main/java/com/example/hexagonal/domain/port/IRestClient.java
@@ -2,9 +2,8 @@ package com.example.hexagonal.domain.port;
 
 import java.util.List;
 import java.util.Optional;
-public interface IRestClient<T> {
 
+public interface IRestClient<T> {
     List<T> getData();
     Optional<T> getDataById(Integer id);
-    
 }
